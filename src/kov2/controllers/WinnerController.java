021e6433378397e99ffb2b7f67f2b0129794329b
Elena Chaya <elenachaya@012smile.net>
2017-12-13 23:21:06 +0200

    fixes for WinnerController (were fixed the issues, that were discovered durring unit testing)

--- src/kov2/controllers/WinnerController.java
+++ src/kov2/controllers/WinnerController.java
@@ -24,7 +24,7 @@ public class WinnerController {
         }
             } 
             for (int i= 0; i<size; i++){
-        if (check(field, new Point(i,0), p -> new Point(p.x + 1, p.y))){
+        if (check(field, new Point(0, i), p -> new Point(p.x + 1, p.y))){
             return field.getFigure(new Point(0, i));
         }
         
@@ -36,24 +36,27 @@ public class WinnerController {
             return field.getFigure(new Point(1, 1));
         }
         
-    } catch (InvalidPointException e) {
+    } catch (final InvalidPointException e) {
         e.printStackTrace();
     }
         return null;
     }
     
-    private boolean check(final Field field, final Point currentPoint, final IPointGenerator pointGenerator) {
+    boolean check(final Field field, final Point currentPoint, final IPointGenerator pointGenerator) {
         final Figure currentFigure;
         final Figure nextFigure;
         final Point nextPoint = pointGenerator.next(currentPoint);
         try {
         
             currentFigure = field.getFigure(currentPoint);
-            nextFigure = field.getFigure(currentPoint);
-        }catch (InvalidPointException e) {
+            if (currentFigure == null) 
+                return false;
+            nextFigure = field.getFigure(nextPoint);
+        }catch (final InvalidPointException e) {
+            
          return true;
     }  
-        if (currentFigure == null) return false;
+        
         if (currentFigure != nextFigure) return false;
         
         return check(field, nextPoint, pointGenerator);
