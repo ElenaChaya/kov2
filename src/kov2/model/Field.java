dc4a2dd21ff94e3b7845aaa7472d0403ffb1a442
Elena Chaya <elenachaya@012smile.net>
2017-12-13 18:18:45 +0200

    refactorinng field to be size flexible

--- src/kov2/model/Field.java
+++ src/kov2/model/Field.java
@@ -14,12 +14,19 @@ import kov2.model.exceptions.InvalidPointException;
  * @author lena
  */
 public class Field {
-    private static final int FIELD_SIZE = 3;
+    
     private static final int MIN_COORDINATE = 0;
-    private static final int MAX_COORDINATE =FIELD_SIZE;
-    private final Figure[][] field = new Figure[FIELD_SIZE][FIELD_SIZE];
+    
+    private final Figure[][] field;
+    private final int field_size;
+
+    public Field(final int field_size) {
+        this.field_size = field_size;
+         this.field = new Figure[field_size][field_size];
+    }
+    
     public int getSize() {
-        return FIELD_SIZE;
+        return field_size;
     }
 
     public Figure getFigure(final Point point) throws InvalidPointException {
@@ -37,11 +44,11 @@ public class Field {
     }
     
     private boolean checkPoint(final Point point) {
-            return checkCoordinate(point.x) && checkCoordinate(point.y);
+            return checkCoordinate(point.x, field.length) && checkCoordinate(point.y, field[point.x].length);
         }
     
-    private boolean checkCoordinate(final int coordinate) {
-        return coordinate >= MIN_COORDINATE && coordinate < MAX_COORDINATE;
+    private boolean checkCoordinate(final int coordinate, final int maxCoordinate) {
+        return coordinate >= MIN_COORDINATE && coordinate < maxCoordinate;
     }
     
 }
