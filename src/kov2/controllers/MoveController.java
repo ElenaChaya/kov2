5e18515cb37f1cf147d19962155065872fefb2e7
Elena Chaya <elenachaya@012smile.net>
2017-12-13 14:40:04 +0200

    initial MoveController

--- src/kov2/controllers/MoveController.java
+++ src/kov2/controllers/MoveController.java
@@ -5,13 +5,22 @@
  */
 package kov2.controllers;
 
+import java.awt.Point;
+import kov2.model.Field;
+import kov2.model.Figure;
+import kov2.model.exceptions.AlreadyOccupiedException;
+import kov2.model.exceptions.InvalidPointException;
+
 /**
  *
  * @author lena
  */
 public class MoveController {
-    public void applyPicture() {
-        System.out.println("test2");
-        System.out.println("test4");
+    public void applyPicture(final Field field, final Point point, final Figure figure) throws InvalidPointException,
+                                                                          AlreadyOccupiedException  {
+        if(field.getFigure(point)!=null)  {
+            throw new AlreadyOccupiedException();
+        }
+        field.setFigure(point, figure);
     }
 }
